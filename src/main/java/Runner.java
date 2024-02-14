c664dc16e3e4f1070da0cb09ea22fe88f34255fd
Lenovo <begalyaish@gmail.com>
2024-02-15 01:56:53 +0600

    change User and Main class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,17 +1,19 @@
 public class Runner {
+
         public static void main(String[] args) {
             // Создаем нового пользователя
-            User user = new User("john_doe");
+            User user = new User("john_doe", "john@example.com");
 
-            // Получаем и выводим имя пользователя
-            String username = user.getUsername();
-            System.out.println("Username: " + username);
+            // Выводим информацию о пользователе
+            System.out.println("Username: " + user.getUsername());
+            System.out.println("Email: " + user.getEmail());
 
-            // Меняем имя пользователя
+            // Меняем имя пользователя и email
             user.setUsername("new_username");
+            user.setEmail("new_email@example.com");
 
-            // Получаем и выводим обновленное имя пользователя
-            String updatedUsername = user.getUsername();
-            System.out.println("Updated Username: " + updatedUsername);
+            // Выводим обновленную информацию о пользователе
+            System.out.println("Updated Username: " + user.getUsername());
+            System.out.println("Updated Email: " + user.getEmail());
         }
     }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,8 +1,10 @@
 public class User {
     private String username;
+    private String email;
 
-    public User(String username) {
+    public User(String username, String email) {
         this.username = username;
+        this.email = email;
     }
 
     public String getUsername() {
@@ -12,5 +14,12 @@ public class User {
     public void setUsername(String username) {
         this.username = username;
     }
-}
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
